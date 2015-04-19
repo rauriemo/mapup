99433b6447f93b295e9f5bae96b3b82b46d75c97
rauriemo <rafael.auriemo@gmail.com>
2015-04-19 01:11:32 -0700

    map and profile pic working

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "instagram"
+require "pp"
 
 enable :sessions
 
@@ -7,7 +8,8 @@ enable :sessions
 CALLBACK_URL = "http://localhost:9393/oauth/callback"
 
 Instagram.configure do |config|
-  # txt
+  config.client_id = "98808b1116e84d428f75eff3aafa9637"
+  config.client_secret = "def45127fa5840878ee4e2ee6cd1cfd5"
 end
 
 # redirect to instagram oath
@@ -15,7 +17,7 @@ get "/" do
   redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
 end
 
-# have href to this if decide to have homepage with link
+# have href to this if decide to have homepage with
 # # on authorize send callback url
 # get "/oauth/connect" do
 #   redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
@@ -30,6 +32,8 @@ end
 
 # index page
 get "/index" do
+  client = Instagram.client(access_token: session[:access_token])
+  @user = client.user
   erb :index
 end
 
@@ -43,9 +47,13 @@ end
 get "/user_recent_media" do
   client = Instagram.client(access_token: session[:access_token])
   user = client.user
-  p user
+  p "client"
+  pp client
+  p "user: "
+  pp user
   html = "<h1>#{user.username}'s recent media</h1>"
   for media_item in client.user_recent_media
+    # p media_item
     html << "<div style='float:left;'><img src='#{media_item.images.thumbnail.url}'><br/> <a href='/media_like/#{media_item.id}'>Like</a>  <a href='/media_unlike/#{media_item.id}'>Un-Like</a>  <br/>LikesCount=#{media_item.likes[:count]}</div>"
   end
   html
