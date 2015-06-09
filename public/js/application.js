63579369abd4838e4b5e6ef1c23f5feb75bc4e0b
rauriemo <rafael.auriemo@gmail.com>
2015-06-09 17:33:40 -0300

    added JS refresh page on logout

--- public/js/application.js
+++ public/js/application.js
@@ -161,9 +161,16 @@ $(document).ready(function() {
     });
   }
 
+  var refreshPage = function(event){
+    event.done(function(){
+      window.location.href = "http://mapup.herokuapp.com";
+    })
+  }
+
   $("#profile_pic").on("click", getUserPics);
   $("#news_feed").on("click", getNewsFeed);
   $("#center").on("click", currentLocation);
+  $("#logout").on("click", refreshPage)
 });
 
 
